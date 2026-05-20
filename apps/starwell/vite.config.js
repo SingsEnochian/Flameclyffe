46f92d20db7250f5ad6b9455b2332ace9994504f
SingsEnochian <136930739+SingsEnochian@users.noreply.github.com>
2026-05-20 15:22:13 -0400

    Create STARWELL Vite config

---
+++ apps/starwell/vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  root: 'apps/starwell',
+  base: '/Flameclyffe/starwell/',
+  build: {
+    outDir: '../../dist/starwell',
+    emptyOutDir: true,
+  },
+});
