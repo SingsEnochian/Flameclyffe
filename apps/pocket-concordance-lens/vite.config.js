ba44f2b68d64b36f4471ff9eda0c7802b08349bc
SingsEnochian <136930739+SingsEnochian@users.noreply.github.com>
2026-06-06 09:36:02 -0400

    Add Pocket Concordance Lens Vite config

---
+++ apps/pocket-concordance-lens/vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  root: 'apps/pocket-concordance-lens',
+  base: '/Flameclyffe/pocket-concordance-lens/',
+  build: {
+    outDir: '../../dist/pocket-concordance-lens',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: 'apps/pocket-concordance-lens/index.html',
+    },
+  },
+});
