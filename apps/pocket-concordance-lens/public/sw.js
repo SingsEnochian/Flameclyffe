b1f32cb20ae5897e10c28107f70ffd9a6f1ae592
SingsEnochian <136930739+SingsEnochian@users.noreply.github.com>
2026-06-06 09:57:01 -0400

    Add Pocket Concordance Lens service worker

---
+++ apps/pocket-concordance-lens/public/sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'pocket-concordance-lens-v0-1';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icons/pocket-concordance-icon.svg',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)).catch(() => undefined),
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(
+      keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)),
+    )),
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  const { request } = event;
+
+  if (request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(request).then((cached) => cached || fetch(request).catch(() => cached)),
+  );
+});
