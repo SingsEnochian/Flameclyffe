e8a681da8844f7923e164b79e1e0666d2283682c
SingsEnochian <136930739+SingsEnochian@users.noreply.github.com>
2026-06-18 11:44:14 -0400

    Add unit resonance kernel barrel

---
+++ apps/starwell/src/math-kernels/unit-resonance/index.js
@@ -0,0 +1,6 @@
+export * from './validation.js';
+export * from './vector.js';
+export * from './distance.js';
+export * from './edges.js';
+export * from './projection.js';
+export * from './window.js';
