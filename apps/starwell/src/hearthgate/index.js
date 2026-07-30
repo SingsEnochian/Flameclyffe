6da1cb302c6be01e41bb9f5d2e948f837265327e
Rowan <136930739+SingsEnochian@users.noreply.github.com>
2026-07-30 13:57:23 -0400

    feat(hearthgate): expose Ring 0 foundation API

---
+++ apps/starwell/src/hearthgate/index.js
@@ -0,0 +1,4 @@
+export { HEARTHGATE_CONTRACT_VERSION, definePracticePackage, defineReceptionProfile, defineRoom, roomRegions } from './contracts.js';
+export { terraAeternaProfile } from './profiles/terra-aeterna.js';
+export { practicePackages, getPracticePackage, resolveProfilePackages } from './packages/registry.js';
+export { hearthgateRooms, getRoom } from './rooms/registry.js';
