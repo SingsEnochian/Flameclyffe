d18f7dd07b9d636aca23eaf23c30b07358b88157
Rowan <136930739+SingsEnochian@users.noreply.github.com>
2026-08-02 01:47:01 -0400

    Export Hearthweave dual-aspect kernel

---
+++ apps/starwell/src/hearthweave-kernel/index.js
@@ -0,0 +1,45 @@
+export {
+  CLAIM_STATES,
+  DEEP_MODES,
+  DUAL_ASPECT_ACTIVATION_EVENT,
+  DUAL_ASPECT_ACTIVE_PACKET_KEY,
+  DUAL_ASPECT_DEACTIVATION_EVENT,
+  DUAL_ASPECT_PACKET_SCHEMA,
+  DUAL_ASPECT_PACKET_VERSION,
+  DUAL_ASPECT_RECEIPT_LEDGER_KEY,
+  DUAL_ASPECT_RECEIPT_SCHEMA,
+  DualAspectError,
+  assembleDualAspectPacket,
+  canonicalJson,
+  createDegradedDeepSnapshot,
+  deepSnapshotToPremaqV2,
+  deriveExperientialState,
+  fingerprint,
+  replayDualAspectPacket,
+  validateDeepSnapshot,
+  validateDualAspectPacket,
+} from './dual-aspect.js';
+
+export {
+  clearDualAspectActivation,
+  publishDualAspectActivation,
+  readActiveDualAspectPacket,
+  readDualAspectReceipt,
+  readDualAspectReceiptForPacket,
+  recordDualAspectRender,
+  recordDualAspectReplay,
+  subscribeToDualAspectActivation,
+} from './activation.js';
+
+export {
+  HEARTHWEAVE_HAPTIC_ACTIVATION_EVENT,
+  HEARTHWEAVE_NARRATIVE_ACTIVATION_EVENT,
+  RUNA_TONE_ACTIVATION_EVENT,
+  SENSORY_ACTIVATION_KEY,
+  acknowledgeSensoryRender,
+  buildSensoryActivation,
+  getActiveSensoryActivation,
+  getPacketForSensoryAcknowledgement,
+  installSensoryActivationBus,
+  publishSensoryActivation,
+} from './sensory-bus.js';
