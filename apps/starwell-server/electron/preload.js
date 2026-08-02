68d97045743e5fd4a0608e369b0eb6c5dac41f81
Rowan <136930739+SingsEnochian@users.noreply.github.com>
2026-08-02 15:04:36 -0400

    Windows House: expose constrained Bifröst launcher

--- apps/starwell-server/electron/preload.js
+++ apps/starwell-server/electron/preload.js
@@ -2,7 +2,8 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
-  getConfig:    ()    => ipcRenderer.invoke('get-config'),
-  saveConfig:   (cfg) => ipcRenderer.invoke('save-config', cfg),
-  openWizard:   ()    => ipcRenderer.invoke('open-wizard'),
+  getConfig:              ()    => ipcRenderer.invoke('get-config'),
+  saveConfig:             (cfg) => ipcRenderer.invoke('save-config', cfg),
+  openWizard:             ()    => ipcRenderer.invoke('open-wizard'),
+  launchBifrostTerminal:  ()    => ipcRenderer.invoke('launch-bifrost-terminal'),
 });
