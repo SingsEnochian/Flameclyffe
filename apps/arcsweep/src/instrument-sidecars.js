1e37efbfaa7530bb2e86f7e638ece4d9bca276c0
Rowan <136930739+SingsEnochian@users.noreply.github.com>
2026-08-13 01:11:05 -0400

    Mount React-ion semantic response console

--- apps/arcsweep/src/instrument-sidecars.js
+++ apps/arcsweep/src/instrument-sidecars.js
@@ -6,3 +6,4 @@ import './react-ion-flight-recorder-sidecar.js';
 import './react-ion-registry-sidecar.js';
 import './react-ion-persistence-sidecar.js';
 import './react-ion-route-map-sidecar.js';
+import './react-ion-response-console-sidecar.js';
