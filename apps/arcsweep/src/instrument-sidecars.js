2a00d4c5c5b80e86137bb1a64b42ba8630c6cc89
Rowan <136930739+SingsEnochian@users.noreply.github.com>
2026-08-13 01:02:00 -0400

    Remove obsolete Helm DNS compatibility shim

--- apps/arcsweep/src/instrument-sidecars.js
+++ apps/arcsweep/src/instrument-sidecars.js
@@ -2,6 +2,5 @@ import './typing-sonifier-sidecar.js';
 import './cusp-bench-sidecar.js';
 import './transformation-request-sidecar.js';
 import './react-ion-helm-sidecar.js';
-import './react-ion-helm-dns-compat.js';
 import './react-ion-flight-recorder-sidecar.js';
 import './react-ion-registry-sidecar.js';
