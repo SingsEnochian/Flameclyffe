823a2c048eae90bb57e6500bbb96e6e7bf0705b3
Rowan <136930739+SingsEnochian@users.noreply.github.com>
2026-08-13 00:55:18 -0400

    Mount React-ion dimensional registry sidecar

--- apps/arcsweep/src/instrument-sidecars.js
+++ apps/arcsweep/src/instrument-sidecars.js
@@ -2,3 +2,4 @@ import './typing-sonifier-sidecar.js';
 import './cusp-bench-sidecar.js';
 import './transformation-request-sidecar.js';
 import './react-ion-helm-sidecar.js';
+import './react-ion-registry-sidecar.js';
