c10995dae83aaf45b7b21d01742c840b8ac1dc58
Rowan <136930739+SingsEnochian@users.noreply.github.com>
2026-08-13 02:08:26 -0400

    Seed React-ion atlas before mounting Helm

--- apps/arcsweep/src/instrument-sidecars.js
+++ apps/arcsweep/src/instrument-sidecars.js
@@ -1,6 +1,7 @@
 import './typing-sonifier-sidecar.js';
 import './cusp-bench-sidecar.js';
 import './transformation-request-sidecar.js';
+import './react-ion-first-flight-atlas-sidecar.js';
 import './react-ion-helm-sidecar.js';
 import './react-ion-flight-recorder-sidecar.js';
 import './react-ion-registry-sidecar.js';
