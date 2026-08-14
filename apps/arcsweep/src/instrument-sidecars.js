49270a8e43a626692e1f4a3772d7a2dda41d0176
Rowan <136930739+SingsEnochian@users.noreply.github.com>
2026-08-14 11:32:33 -0400

    feat(arcsweep): mount Runa preview observation closure

--- apps/arcsweep/src/instrument-sidecars.js
+++ apps/arcsweep/src/instrument-sidecars.js
@@ -10,5 +10,6 @@ import './theory-review-advisor-sidecar.js';
 import './deep-time-trajectory-sidecar.js';
 import './runa-trajectory-sidecar.js';
 import './runa-renderer-sidecar.js';
+import './runa-preview-observation-link-sidecar.js';
 import './receipt-provenance-sidecar.js';
 import './receipt-integrity-sidecar.js';
