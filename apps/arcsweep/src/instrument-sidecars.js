6a53312d4b0e07dbadd850ea4de245e23388b3e7
Rowan <136930739+SingsEnochian@users.noreply.github.com>
2026-08-14 11:35:32 -0400

    feat(arcsweep): mount Runa observation return

--- apps/arcsweep/src/instrument-sidecars.js
+++ apps/arcsweep/src/instrument-sidecars.js
@@ -11,5 +11,6 @@ import './deep-time-trajectory-sidecar.js';
 import './runa-trajectory-sidecar.js';
 import './runa-renderer-sidecar.js';
 import './runa-preview-observation-link-sidecar.js';
+import './runa-preview-return-sidecar.js';
 import './receipt-provenance-sidecar.js';
 import './receipt-integrity-sidecar.js';
