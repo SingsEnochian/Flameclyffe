c7cebe0e1b56203699c20ad2f1b17892c9fc2e62
Rowan <136930739+SingsEnochian@users.noreply.github.com>
2026-08-14 01:58:05 -0400

    Mount Glyph Drift Observatory

--- apps/arcsweep/src/instrument-sidecars.js
+++ apps/arcsweep/src/instrument-sidecars.js
@@ -14,3 +14,4 @@ import './react-ion-world-tone-sync-sidecar.js';
 import './react-ion-concordance-anchor-sidecar.js';
 import './react-ion-replay-room-sidecar.js';
 import './react-ion-deepstory-sidecar.js';
+import './glyph-drift-observatory-sidecar.js';
