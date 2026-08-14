71183c3ed7a8ab9168944a0c85f72558b1ea6539
Rowan <136930739+SingsEnochian@users.noreply.github.com>
2026-08-14 09:39:40 -0400

    feat(arcsweep): add domain control bench and bidirectional sweeps

--- apps/arcsweep/src/instrument-sidecars.js
+++ apps/arcsweep/src/instrument-sidecars.js
@@ -2,3 +2,4 @@ import './typing-sonifier-sidecar.js';
 import './cusp-bench-sidecar.js';
 import './transformation-request-sidecar.js';
 import './bai-topology-sidecar.js';
+import './domain-control-bench-sidecar.js';
