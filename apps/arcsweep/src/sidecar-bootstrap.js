4be8a29e484f945cbf0148466b501377b6c4969b
Rowan <136930739+SingsEnochian@users.noreply.github.com>
2026-08-27 20:51:34 -0400

    refactor: prefer push delivery over browser mailbox polling

--- apps/arcsweep/src/sidecar-bootstrap.js
+++ apps/arcsweep/src/sidecar-bootstrap.js
@@ -19,7 +19,6 @@ const SIDECARS = [
   './runtime-integration-bootstrap.js',
   './hosted-house-session-ui.js',
   './house-commons-chat-v3.js',
-  './lanternbridge-mailbox-live.js',
   './house-commons-command-room.js',
   './house-commons-thread-restoration.js',
   './house-commons-deep-link-router.js',
